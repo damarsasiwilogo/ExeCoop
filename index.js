20bd826c05e9b6d2acfe7c8e40485207ae289266
anggaratriputra <anggaratriputra@gmail.com>
2023-08-14 13:24:30 +0700

    adding prompt menu

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-console.log("test");
-console.log("hai");
-
+function showPrompt() {
+    const userInput = window.prompt(`
+    Pilih menu :
+    1. Penjumlahan
+    2. Pengurangan
+    3. Pembagian
+    4. Perkalian
+    5. Pangkat 2
+    6. Akar pangkat 2
+    7. Akar pangkat 3
+    `);
+}
